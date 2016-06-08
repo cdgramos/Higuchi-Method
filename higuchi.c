345cc46be3fcbc27919083149a3cd9c949ed2884
cdgramos <cdgramos@outlook.com>
2016-06-08 22:18:14 +0100

    under dev

--- higuchi.c
+++ higuchi.c
@@ -16,8 +16,13 @@ int main(){
 	int N = 0; // number of data records
 
 	int i = 0; // general purpose
+	int j = 0; // general purpose
+    double aux = 0.0; // general purpose
 
-	double *sequence; // store input / raw data
+    int mLarge = 0;
+
+	double *x; // store input / raw data
+	double *sequence; // treated data
 
     printf("Opening the raw data file...\n");
     fp = fopen("hurst_050.txt","r");
@@ -34,8 +39,9 @@ int main(){
 
     printf("Allocating memory...\n");
     sequence = (double *) calloc(N+1, sizeof(double));
+    x = (double *) calloc(N+1, sizeof(double));
 
-    if(sequence == NULL){
+    if(sequence == NULL || x == NULL){
         printf( "\t\tError: allocating memory\n");
         exit(1);
     }
@@ -43,9 +49,20 @@ int main(){
     printf("Reading raw data and closing the raw data file...\n");
     i = 1;
     while(i <= N){
-        fscanf(fp,"%lf\n",&sequence[i]);
+        fscanf(fp,"%lf\n",&x[i]);
         i++;
     }
+
+    printf("Cumulative sums...\n");
+    for(i=1; i<=N; i++){
+        aux = 0.0;
+        for(j=1; j<=i; j++){
+            aux+=x[j];
+        }
+        sequence[i] = aux;
+    }
+
+    mLarge = floor(N/5); //5 is an empirical value!
 
     printf("Freeing memory...\n");
     free(sequence);
