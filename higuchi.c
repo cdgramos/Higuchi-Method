348c09c0e05b463e488cc711a38112b7948f6fbd
cdgramos <cdgramos@outlook.com>
2016-06-08 22:12:50 +0100

    Revert "under dev"
    This reverts commit 58ba72ec91143785d1e142d370b42775576af8fc.

--- higuchi.c
+++ higuchi.c
@@ -16,11 +16,8 @@ int main(){
 	int N = 0; // number of data records
 
 	int i = 0; // general purpose
-	int j = 0; // general purpose
-    double aux = 0.0; // general purpose
 
-	double *x; // store input / raw data
-	double *sequence; // treated data
+	double *sequence; // store input / raw data
 
     printf("Opening the raw data file...\n");
     fp = fopen("hurst_050.txt","r");
@@ -37,9 +34,8 @@ int main(){
 
     printf("Allocating memory...\n");
     sequence = (double *) calloc(N+1, sizeof(double));
-    x = (double *) calloc(N+1, sizeof(double));
 
-    if(sequence == NULL || x == NULL){
+    if(sequence == NULL){
         printf( "\t\tError: allocating memory\n");
         exit(1);
     }
@@ -47,17 +43,8 @@ int main(){
     printf("Reading raw data and closing the raw data file...\n");
     i = 1;
     while(i <= N){
-        fscanf(fp,"%lf\n",&x[i]);
+        fscanf(fp,"%lf\n",&sequence[i]);
         i++;
-    }
-
-    printf("Cumulative sums...\n");
-    for(i=1; i<=N; i++){
-        aux = 0.0;
-        for(j=1; j<=i; j++){
-            aux+=x[j];
-        }
-        sequence[i] = aux;
     }
 
     printf("Freeing memory...\n");
